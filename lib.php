e96d616adbe47a6a2541e3a5968830418964bd90
Nadav Kavalerchik <nadavkav@gmail.com>
2017-03-14 23:30:06 +0200

    + Fix search + Fix paging + Code cleanup

--- lib.php
+++ lib.php
@@ -23,7 +23,7 @@
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 require_once($CFG->dirroot . '/repository/lib.php');
-
+//use curl;
 /**
  * repository_wpvod class
  *
@@ -35,7 +35,7 @@ require_once($CFG->dirroot . '/repository/lib.php');
 
 class repository_wpvod extends repository {
     /** @var int maximum number of thumbs per page */
-    const wpvod_THUMBS_PER_PAGE = 27;
+    const wpvod_THUMBS_PER_PAGE = 15;
 
     /**
      * API key for using the wpvod Data API.
@@ -43,20 +43,51 @@ class repository_wpvod extends repository {
      */
     private $apikey;
 
-     /**
-     * wpvod plugin constructor
-     * @param int $repositoryid
-     * @param object $context
-     * @param array $options
+    /**
+     * Internal Wordpress VOD REST API URL.
+     * @var mixed
      */
+    private $wpresturl;
+
+    /**
+     * Public Wordpress VOD - KALTURA streamer NGINX plug settings URL.
+     * @var mixed
+     */
+    private $wpvodurl;
+
+    /**
+     * Wordpress RESTFul API v2 orderby types
+     * https://v2.wp-api.org/reference/media/
+     * 
+     */
+    private $orderbytypes = array('relevance' => 'relevance', 'date' => 'date', 'title' => 'title', 'id' => 'id');
+
+    /**
+    * wpvod plugin constructor
+    * @param int $repositoryid
+    * @param object $context
+    * @param array $options
+    */
     public function __construct($repositoryid, $context = SYSCONTEXTID, $options = array()) {
         parent::__construct($repositoryid, $context, $options);
 
         $this->apikey = $this->get_option('apikey');
 
-        // Without an API key, don't show this repo to users as its useless without it.
-        if (empty($this->apikey)) {
-            $this->disabled = true;
+        // Internal URL, not open to the world
+        // needs to be translated to a public URL,
+        // specially setup to use KALTURA streamer NGINX plug settings
+        //
+        // NOTE: If you do not seperate public and internal networks,
+        //       both "wpresturl" and "wpvodurl" should be the same URL
+        $this->wpresturl = get_config('wpvod', 'wpresturl');
+        if (empty($this->wpresturl)) {
+            $this->wpresturl = 'http://davidsonvod.weizmann.ac.il:8080/wordpress';
+        }
+        // KALTURA streamer NGINX plug settings URL
+        // https://gist.github.com/nadavkav/56f227572afd0c82d06425a79151886c#file-usr_local_nginx_conf_nginx-conf-L145
+        $this->wpvodurl = get_config('wpvod', 'wpvodurl');
+        if (empty($this->wpvodurl)) {
+            $this->wpvodurl = 'https://davidsonvod.weizmann.ac.il/vod';
         }
     }
 
@@ -95,29 +126,36 @@ class repository_wpvod extends repository {
     /**
      * Return search results
      * @param string $search_text
+     * @param int $page
      * @return array
      */
     public function search($search_text, $page = 0) {
         global $SESSION;
-        $sort = optional_param('wpvod_sort', '', PARAM_TEXT);
+        $orderby = optional_param('wpvod_orderby', '', PARAM_TEXT);
         $mode = optional_param('wpvod_mode', '', PARAM_TEXT);
-        $sess_keyword = 'wpvod_'.$this->id.'_keyword';
-        $sess_sort = 'wpvod_'.$this->id.'_sort';
 
-        // This is the request of another page for the last search, retrieve the cached keyword and sort
+        $sess_keyword = 'wpvod_'.$this->id.'_keyword';
+        $sess_orderby = 'wpvod_'.$this->id.'_orderby';
+        $sess_mode = 'wpvod_'.$this->id.'_mode';
+
+        // This is the request of another page for the last search, retrieve the cached keyword and orderby
         if ($page && !$search_text && isset($SESSION->{$sess_keyword})) {
             $search_text = $SESSION->{$sess_keyword};
         }
-        if ($page && !$sort && isset($SESSION->{$sess_sort})) {
-            $sort = $SESSION->{$sess_sort};
+        if ($page && !$orderby && isset($SESSION->{$sess_orderby})) {
+            $orderby = $SESSION->{$sess_orderby};
         }
-        if (!$sort) {
-            $sort = 'relevance'; // default
+        if (!$orderby) {
+            $orderby = 'relevance'; // default
+        }
+        if ($page && !$mode && isset($SESSION->{$sess_mode})) {
+            $mode = $SESSION->{$sess_mode};
         }
 
         // Save this search in session
         $SESSION->{$sess_keyword} = $search_text;
-        $SESSION->{$sess_sort} = $sort;
+        $SESSION->{$sess_orderby} = $orderby;
+        $SESSION->{$sess_mode} = $mode;
 
         $this->keyword = $search_text;
         $ret  = array();
@@ -126,10 +164,10 @@ class repository_wpvod extends repository {
         if ($ret['page'] < 1) {
             $ret['page'] = 1;
         }
-        $start = ($ret['page'] - 1) * self::wpvod_THUMBS_PER_PAGE + 1;
+        $start = ($ret['page'] - 1) * self::wpvod_THUMBS_PER_PAGE;
         $max = self::wpvod_THUMBS_PER_PAGE;
-        $ret['list'] = $this->_get_collection($search_text, $start, $max, $sort, $mode);
-        $ret['norefresh'] = true;
+        $ret['list'] = $this->_get_collection($search_text, $start, $max, $orderby, $mode);
+        //$ret['norefresh'] = true;
         $ret['nosearch'] = true;
         // If the number of results is smaller than $max, it means we reached the last page.
         $ret['pages'] = (count($ret['list']) < $max) ? $ret['page'] : -1;
@@ -141,40 +179,40 @@ class repository_wpvod extends repository {
      * @param string $keyword
      * @param int $start
      * @param int $max max results
-     * @param string $sort
+     * @param string $orderby
      * @param string $mode
      * @throws moodle_exception If the google API returns an error.
      * @return array
      */
-    private function _get_collection($keyword, $start, $max, $sort, $mode) {
-        global $SESSION;
-
-        // The new API doesn't use "page" numbers for browsing through results.
-        // It uses a prev and next token in each set that you need to use to
-        // request the next page of results.
-        $sesspagetoken = 'wpvod_'.$this->id.'_nextpagetoken';
-        $pagetoken = '';
-        if ($start > 1 && isset($SESSION->{$sesspagetoken})) {
-            $pagetoken = $SESSION->{$sesspagetoken};
-        }
+    private function _get_collection($keyword, $start, $max, $orderby, $mode) {
+        global $DB, $SESSION;
 
         $list = array();
         $error = null;
-
-        $params = array('action' => 'search', 'subject' => $keyword);
-        $wsurl = 'http://davidsonvod.weizmann.ac.il:8080/wordpress/wp-json/wp/v2/media';
 
         list($context, $course, $cm) = get_context_info_array($this->context->id);
         $courseid = is_object($course) ? $course->id : SITEID;
         //$context = context_course::instance($courseid);
 
+        // https://v2.wp-api.org/reference/media/
+        //
+        if (empty($keyword) && $mode == 'currentcourse') {
+            $keyword = $courseid;
+        }
+        // filter[wp_query-arguments] - http://www.billerickson.net/code/wp_query-arguments/
+        //$params = array('search' => $keyword, 'offset' => $start, 'per_page' => $max, 'orderby' => $orderby, 'mime_type' => 'video/mp4');
+        $params = array('search' => $keyword, 'offset' => $start, 'per_page' => $max, 'orderby' => $orderby, 'mime_type' => 'video/mp4');
+        //$params = array('search' => $keyword, 'offset' => $start);
+        $wsurl = $this->wpresturl.'/wp-json/wp/v2/media';
+
         // TODO: Use Moodle's cURL
-        //$curl_vodlist= new curl(array('proxy' => false));
-        //$vodlist_json = $curl_vodlist->get($wsurl, $params);
-
+        $curl_vodlist= new \curl(array('proxy' => false));
+        $vodlist_json = $curl_vodlist->get($wsurl, $params);
+//var_dump($vodlist_json);die;
         // My cURL
-        $vodlist = $this->ws_get($wsurl);
-
+        //$vodlist = $this->ws_get($wsurl);
+        $vodlist = json_decode($vodlist_json, true);
+//var_dump($vodlist);die;
         foreach ($vodlist as $voditem) {
             //preg_match('/unickoid=(\d{1,4})+/',$voditem['description'], $matches);
             //$unickoid = $matches[1].'<br>';
@@ -189,6 +227,9 @@ class repository_wpvod extends repository {
             // Only get relevant videos for current course. (if mode == currentcourse)
             if (($mode == 'currentcourse') && ($courseid != $mcid)) continue;
 
+            $thiscourse = $DB->get_record('course', array('id' => $mcid));
+            $coursename = $thiscourse->shortname;
+
             if ($voditem['mime_type'] == 'video/mp4'
                 //AND mb_strpos(" ".$voditem['title']['rendered']." ", $keyword)
                 ) {
@@ -200,21 +241,21 @@ class repository_wpvod extends repository {
                 // Get video thumb link.
                 // todo: combine this info into the original WS call. (save network traffic)
                 $json_thimblink = $this->ws_get($voditem['_links']['wp:featuredmedia'][0]['href']);
-                $thumblink = str_replace('http://davidsonvod.weizmann.ac.il/wordpress/wp-content/uploads',
-                    'https://davidsonvod.weizmann.ac.il/vod', $json_thimblink['guid']['rendered']);
+                //$thumblink = str_replace('http://davidsonvod.weizmann.ac.il/wordpress/wp-content/uploads',
+                //    'https://davidsonvod.weizmann.ac.il/vod', $json_thimblink['guid']['rendered']);
                 $thumblink = str_replace('http://', 'https://', $json_thimblink['guid']['rendered']);
                 // Use direct thumbnail link.
                 //$thumblink = $json_thimblink['guid']['rendered'];
 
-                $videolink = str_replace('http://davidsonvod.weizmann.ac.il/wordpress/wp-content/uploads',
-                    'https://davidsonvod.weizmann.ac.il/vod', $voditem['guid']['rendered']);
+                $videolink = str_replace($this->wpresturl.'/wp-content/uploads',
+                    $this->wpvodurl, $voditem['guid']['rendered']);
 
                 $list[] = array(
                     //'shorttitle' => $voditem['title']['rendered'],
                     //'shorttitle' => $voditem['media_details']['source_url'],
-                    'thumbnail_title' => $voditem['title']['rendered'],
+                    'thumbnail_title' => $voditem['title']['rendered']. ' - '.$coursename,
                     //'thumbnail_title' => $voditem['media_details']['source_url'],
-                    'title' => $voditem['title']['rendered'], // This is a hack so we accept this file by extension.
+                    'title' => $voditem['title']['rendered']. ' - '.$coursename, // This is a hack so we accept this file by extension.
                     //'description' => $voditem['description'], // This is a hack so we accept this file by extension.
                     'thumbnail' => $thumblink,
                     'thumbnail_width' => 150, //(int)$thumb->width,
@@ -294,31 +335,35 @@ class repository_wpvod extends repository {
         $mode->name = 'wpvod_mode';
         $mode->label = get_string('mode', 'repository_wpvod').': ';
 
-        $sort = new stdClass();
-        $sort->type = 'select';
-        $sort->options = array(
+        $orderby = new stdClass();
+        $orderby->type = 'select';
+        $orderby->options = array(
             (object)array(
                 'value' => 'relevance',
-                'label' => get_string('sortrelevance', 'repository_wpvod')
+                'label' => get_string('orderbyrelevance', 'repository_wpvod')
             ),
             (object)array(
                 'value' => 'date',
-                'label' => get_string('sortpublished', 'repository_wpvod')
+                'label' => get_string('orderbypublished', 'repository_wpvod')
+            ),
+            (object)array(
+                'value' => 'title',
+                'label' => get_string('orderbytitle', 'repository_wpvod')
             ),
             (object)array(
                 'value' => 'rating',
-                'label' => get_string('sortrating', 'repository_wpvod')
+                'label' => get_string('orderbyrating', 'repository_wpvod')
             ),
             (object)array(
                 'value' => 'viewCount',
-                'label' => get_string('sortviewcount', 'repository_wpvod')
+                'label' => get_string('orderbyviewcount', 'repository_wpvod')
             )
         );
-        $sort->id = 'wpvod_sort';
-        $sort->name = 'wpvod_sort';
-        $sort->label = get_string('sortby', 'repository_wpvod').': ';
+        $orderby->id = 'wpvod_orderby';
+        $orderby->name = 'wpvod_orderby';
+        $orderby->label = get_string('orderby', 'repository_wpvod').': ';
 
-        $ret['login'] = array($search, $mode, $sort);
+        $ret['login'] = array($search, $mode, $orderby);
         $ret['login_btn_label'] = get_string('search');
         $ret['login_btn_action'] = 'search';
         $ret['allowcaching'] = true; // indicates that login form can be cached in filepicker.js
@@ -362,9 +407,35 @@ class repository_wpvod extends repository {
             $apikey = '';
         }
 
+        // Internal URL, not open to the world
+        // needs to be translated to a public URL,
+        // specially setup to use KALTURA streamer NGINX plug settings
+        //
+        // NOTE: If you do not seperate public and internal networks,
+        //       both "wpresturl" and "wpvodurl" should be the same URL
+        $wpresturl = get_config('wpvod', 'wpresturl');
+        if (empty($wpresturl)) {
+            $wpresturl = 'http://davidsonvod.weizmann.ac.il:8080/wordpress';
+        }
+        // KALTURA streamer NGINX plug settings URL
+        // https://gist.github.com/nadavkav/56f227572afd0c82d06425a79151886c#file-usr_local_nginx_conf_nginx-conf-L145
+        $wpvodurl = get_config('wpvod', 'wpvodurl');
+        if (empty($wpvodurl)) {
+            $wpvodurl = 'https://davidsonvod.weizmann.ac.il/vod';
+        }
+
+        // TODO: use "apikey" to "authenticate" the connection between Moodle and WP-VOD (REST APIs)
         $mform->addElement('text', 'apikey', get_string('apikey', 'repository_wpvod'), array('value' => $apikey, 'size' => '40'));
         $mform->setType('apikey', PARAM_RAW_TRIMMED);
         $mform->addRule('apikey', get_string('required'), 'required', null, 'client');
+
+        $mform->addElement('text', 'wpresturl', get_string('wpresturl', 'repository_wpvod'), array('value' => $wpresturl, 'size' => '70'));
+        $mform->setType('wpresturl', PARAM_RAW_TRIMMED);
+        $mform->addRule('wpresturl', get_string('required'), 'required', null, 'client');
+
+        $mform->addElement('text', 'wpvodurl', get_string('wpvodurl', 'repository_wpvod'), array('value' => $wpvodurl, 'size' => '60'));
+        $mform->setType('wpvodurl', PARAM_RAW_TRIMMED);
+        $mform->addRule('wpvodurl', get_string('required'), 'required', null, 'client');
 
         $mform->addElement('static', null, '',  get_string('information', 'repository_wpvod'));
     }
